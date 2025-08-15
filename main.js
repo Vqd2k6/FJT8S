cad5f5728c46353b32364c6ce8b77e2415d67d9e
Vqd2k6 <Vqd2k6@gmail.com>
2025-08-15 17:40:28 +0700

    Học cú pháp comment

--- main.js
+++ main.js
@@ -1,4 +1,14 @@
 var fullName = 'Vo Quang Dung';
-var age = 19;
-alert(fullName); 
-alert(age);
+var age = 19; // comment 1 dòng 
+// alert(fullName); 
+// alert(age);
+
+/**
+ * Đây là comment nhiều dòng có hoa thị hỗ trợ bởi vscode 
+ */
+
+
+/* Đây là comment
+            nhiều                dòng 
+            thông 
+thường*/
